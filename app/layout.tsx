d96fd361ca316da1059fbb2867972f8cb30d3d01
faisalfarizi22 <faisal.alfariziak@gmail.com>
2025-11-05 20:04:05 +0700

    update embed button

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+// layout.tsx
 import type React from "react"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
@@ -8,11 +9,27 @@ import "./globals.css"
 const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
+const APP_URL = "https://thfncstr.vercel.app"
+
 export const metadata: Metadata = {
-  title: "NFT Minting",
+  title: "The Funcaster NFT Mint",
   description: "Simple NFT Minting Page",
   icons: {
     icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "The Funcaster NFT",
+    description: "A Tribute to Warplets Holders. Mint yours now!",
+    images: [`${APP_URL}/image.png`],
+    url: APP_URL,
+    type: "website",
+  },
+  other: {
+    "fc:frame": "vNext",
+    "fc:frame:image": `${APP_URL}/image.png`, 
+    "fc:frame:button:1": "Mint", 
+    "fc:frame:button:1:action": "post_redirect", 
+    "fc:frame:post_url": APP_URL, 
   },
 }
 
